616515e3d04a6522884ce01c5fa559ba224df7b6
burmitskyoa <ole27988967@yandex.ru>
2018-12-01 13:52:36 +0400

    +LS

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -1,46 +1,52 @@
 import { Injectable } from '@angular/core';
-//import { HttpClient } from '@angular/common/http';
-
 import {BehaviorSubject} from 'rxjs';
 import {HttpClient, HttpEvent, HttpRequest, HttpXhrBackend} from '@angular/common/http';
 import {catchError, map} from 'rxjs/operators';
 import {Observable} from 'rxjs';
-
 import { User } from '../models';
+const LOCAL_STORAGE_KEY = 'users';
 
 @Injectable()
 export class UserService {
-	user: User;
 	
-    constructor(private http: HttpClient) { }
+  user: User;
+		
+  constructor(private http: HttpClient) { }
 
-    getAll() {	
-		return this.http.get('http://localhost:3000/listUsers', {responseType: 'json'});
-    }
+  getAll() {	
+	return this.http.get('http://localhost:3000/listUsers', {responseType: 'json'});
+  }
 
-    getById(id: number) {
-        return this.http.get(`http://localhost:3000/` + id);
-    }
+  getById(id: number) {
+    return this.http.get(`http://localhost:3000/` + id);
+  }
     
-	getFIO(id: number) {
-		console.log(id);
-        this.getById(id).subscribe((_user : User) => { 
-            console.log(_user);
-			this.user = _user; 
-        });
-		console.log(this.user);
-		return this.user.firstName;
-    }
+  getFIO(id: number) {	
+    this.user = this.getLocal().find((user,i) => user.id === id);	
+	if (typeof(this.user) != "undefined") {
+	  return this.user.firstName + " " + this.user.lastName;
+	} else {
+	  return "user not found";
+	}
+  }
 	
-    register(user: User) {		
-        return this.http.post(`http://localhost:3000/addUser`, user);
-    }
+  register(user: User) {		
+    return this.http.post(`http://localhost:3000/addUser`, user);
+  }
 
-    /*update(user: User) {
-        return this.http.put(`/users/` + user.id, user);
-    }*/
+  /*update(user: User) {
+    return this.http.put(`/users/` + user.id, user);
+  }*/
 
-    delete(id: number) {
-        return this.http.delete('http://localhost:3000/deleteUser/' + id);
-    }
+  delete(id: number) {
+    return this.http.delete('http://localhost:3000/deleteUser/' + id);
+  }
+	
+  saveLocal(users : User[]) {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(users));	
+  }
+	
+  getLocal() {
+    return JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || [];	
+  }
 }
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -6,27 +6,28 @@ import { UserService } from '../shared/services';
 
 @Component({templateUrl: 'users.component.html'})
 export class UsersComponent implements OnInit {
-    currentUser: User;
-    users: User[] = [];
+  currentUser: User;
+  users: User[] = [];
 
-    constructor(private userService: UserService) {
-        this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
-    }
+  constructor(private userService: UserService) {
+    this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+  }
 
-    ngOnInit() {
-        this.loadAllUsers();
-    }
+  ngOnInit() {
+    this.loadAllUsers();
+  }
 
-    deleteUser(id: number) {
-        this.userService.delete(id).pipe(first()).subscribe(() => { 
-            this.loadAllUsers() 
-        });
-    }
+  deleteUser(id: number) {
+      this.userService.delete(id).pipe(first()).subscribe((_users : User[]) => { 
+        this.users = _users; 
+	    this.userService.saveLocal(_users);
+      });
+  }
 
-    private loadAllUsers() {		
-        this.userService.getAll().pipe(first()).subscribe((_users : User[]) => { 
-            //console.log(_users);
-			this.users = _users; 
-        });
-    }
+  private loadAllUsers() {		
+    this.userService.getAll().pipe(first()).subscribe((_users : User[]) => {             
+	  this.users = _users; 
+	  this.userService.saveLocal(_users);			
+    });
+  }
 }
